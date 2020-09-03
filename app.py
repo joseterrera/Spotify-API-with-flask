e603715e3470cd5924e186acb2c7147fc833e279
Jose Terrera <joseterrera@yahoo.com>
2020-09-02 17:55:45 -0700

    adding search functionality

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-from flask import Flask, render_template, request
-import requests
-import base64
-import datetime
-
-client_id = '2e2cc52be28c4a3dab756d2377edfe72'
-client_secret = 'e75e32f16bed4482b97af54a4249ab94'
 
 class SpotifyAPI(object):
     access_token = None
@@ -48,7 +41,8 @@ class SpotifyAPI(object):
         token_headers = self.get_token_headers()
         r = requests.post(token_url, data=token_data, headers=token_headers)
         if r.status_code not in range(200, 299):
-            return False
+            raise Exception("Could not authenticate client.")
+            # return False
         data = r.json()
         now = datetime.datetime.now()
         access_token = data['access_token']
@@ -58,7 +52,62 @@ class SpotifyAPI(object):
         self.access_token_expires = expires
         self.access_token_did_expire = expires < now
         return True
+    
+    def get_access_token(self):
+        token = self.access_token
+        expires = self.access_token_expires
+        now = datetime.datetime.now()
+        if expires < now:
+            self.perform_auth()
+            return self.get_access_token()
+        elif token == None:
+            self.perform_auth()
+            return self.get_access_token() 
+        return token
+    
+    def get_resource_header(self):
+        access_token = self.get_access_token()
+        headers = {
+            "Authorization": f"Bearer {access_token}"
+        }
+        return headers
+        
+        
+    def get_resource(self, lookup_id, resource_type='albums', version='v1'):
+        endpoint = f"https://api.spotify.com/{version}/{resource_type}/{lookup_id}"
+        headers = self.get_resource_header()
+        r = requests.get(endpoint, headers=headers)
+        if r.status_code not in range(200, 299):
+            return {}
+        return r.json()
+    
+    def get_album(self, _id):
+        return self.get_resource(_id, resource_type='albums')
+    
+    def get_artist(self, _id):
+        return self.get_resource(_id, resource_type='artists')
+    
+    def base_search(self, query_params): # type
+        headers = self.get_resource_header()
+        endpoint = "https://api.spotify.com/v1/search"
+        lookup_url = f"{endpoint}?{query_params}"
+        r = requests.get(lookup_url, headers=headers)
+        if r.status_code not in range(200, 299):  
+            return {}
+        return r.json()
+    
+    def search(self, query=None, operator=None, operator_query=None, search_type='artist' ):
+        if query == None:
+            raise Exception("A query is required")
+        if isinstance(query, dict):
+            query = " ".join([f"{k}:{v}" for k,v in query.items()])
+        if operator != None and operator_query != None:
+            if operator.lower() == "or" or operator.lower() == "not":
+                operator = operator.upper()
+                if isinstance(operator_query, str):
+                    query = f"{query} {operator} {operator_query}"
+        query_params = urlencode({"q": query, "type": search_type.lower()})
+        print(query_params)
+        return self.base_search(query_params)
 
-# spotify = SpotifyAPI(client_id, client_secret)
-# spotify.perform_auth()
-# spotify.access_token
+        
