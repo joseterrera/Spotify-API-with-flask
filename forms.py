0bc6a9b55e132f5770ebae9729bdce48417852a0
Jose Terrera <joseterrera@yahoo.com>
2020-09-04 22:08:05 -0700

    basic playlist version, about to add auth

---
+++ forms.py
@@ -0,0 +1,29 @@
+"""Forms for playlist app."""
+
+from wtforms import SelectField
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SelectField, TextAreaField, BooleanField
+from wtforms.validators import InputRequired, Length, NumberRange, URL, Optional
+
+
+class PlaylistForm(FlaskForm):
+    """Form for adding playlists."""
+    # Add the necessary code to use this form
+    name = StringField("Playlist Name", validators=[InputRequired()])
+    description = StringField('Playlist Description', validators=[InputRequired()])
+
+
+
+
+class SongForm(FlaskForm):
+    """Form for adding songs."""
+    # Add the necessary code to use this form
+    title = StringField("Song Title", validators=[InputRequired()] )
+    artist = StringField("Artist Name", validators=[InputRequired()] )
+
+
+# DO NOT MODIFY THIS FORM - EVERYTHING YOU NEED IS HERE
+class NewSongForPlaylistForm(FlaskForm):
+    """Form for adding a song to playlist."""
+
+    song = SelectField('Song To Add', coerce=int)
--- models.py
+++ models.py
@@ -1,9 +1,15 @@
 """Models for Playlist app."""
 
 from flask_sqlalchemy import SQLAlchemy
+# from flask_login import UserMixin
 
 db = SQLAlchemy()
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
 
 class Playlist(db.Model):
     """Playlist."""
